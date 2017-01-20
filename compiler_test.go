c566104fec978e4278b36d1b43464bece1be8391
William Dussault <dalloriam@gmail.com>
2017-01-20 15:58:14 -0500

    More tests

---
+++ compiler_test.go
@@ -0,0 +1,30 @@
+package opentis100
+
+import "testing"
+
+func TestCompileMov(t *testing.T) {
+	program := `@0
+mov 1 acc
+	`
+
+	p, err := Compile("testProg", program)
+
+	if err != nil {
+		t.Errorf("Unexpected compile error: %s", err.Error())
+	}
+
+	t.Run("Program has right number of sets", func(t *testing.T) {
+		if len(p.Sets) != 1 {
+			t.Errorf("Expected %d set(s), got %d", 1, len(p.Sets))
+		}
+
+	})
+
+	t.Run("Program set has right number of instructions", func(t *testing.T) {
+		set := p.Sets[0]
+
+		if len(set.Instructions) > 1 {
+			t.Errorf("Expected %d instruction(s), got %d", 1, len(set.Instructions))
+		}
+	})
+}
---
+++ instruction_test.go
@@ -0,0 +1,32 @@
+package opentis100
+
+import (
+	"fmt"
+	"testing"
+)
+
+type testCase struct {
+	op         string
+	arg1       string
+	arg2       string
+	expectedOp operation
+}
+
+func TestNewInstruction(t *testing.T) {
+
+	cases := []testCase{
+		testCase{op: "mov", arg1: "1", arg2: "acc", expectedOp: MOV},
+		testCase{op: "sub", arg1: "1", expectedOp: SUB},
+		testCase{op: "add", arg1: "1", expectedOp: ADD},
+	}
+
+	for _, cas := range cases {
+		t.Run(fmt.Sprintf("New %s instruction", cas.op), func(t *testing.T) {
+			i := newInstruction(cas.op, cas.arg1, cas.arg2)
+
+			if i.Operation != cas.expectedOp {
+				t.Errorf("Expected instruction %s, got %s instead", cas.expectedOp, i.Operation)
+			}
+		})
+	}
+}
